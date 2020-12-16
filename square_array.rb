66153080832f407c383160e6e92495e5078fdf80
Lilibeth Santo <itzlilibeth@gmail.com>
2020-12-16 03:08:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  squared_array = []
-   array.each do |element|
-     squared = element * element
-     puts squared
-     squared_array << squared
-   end
-   return squared_array
- end
+  square_array = []
+  array.each do |element|
+    squared = element * element
+    puts squared
+    squared_array << squared
+  end
+  return square_array
+end
